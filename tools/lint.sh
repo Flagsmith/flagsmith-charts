17bbcfa832833a143d43811a3337e6c7bd8006d7
Andrew Plummer <plummer574@gmail.com>
2021-04-22 12:13:26 +0100

    Lint utility: tidy generated charts directories

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -ex
 
 docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing ct lint --config ct.yaml "$@"
+# Tidy up any generated sub-charts directories, which will be owned by root because of the docker container
+docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing sh -c 'rm -rf charts/**/charts'
