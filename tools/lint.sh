8a2f3cbd88709c5b349ffb3f915f3ae1bf93a7ae
Andrew Plummer <plummer574@gmail.com>
2021-04-22 12:55:52 +0100

    Lint utility: even more thorough tidying (#21)

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -ex
 
-docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing ct lint --config ct.yaml "$@"
-# Tidy up any generated sub-charts directories, which will be owned by root because of the docker container
-docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing sh -c 'rm -rf charts/**/charts'
+cleanup() {
+    # Tidy up any generated sub-charts directories, which will be owned by root because of the docker container
+    echo "CLEANING UP"
+    docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing sh -c 'rm -rf charts/**/charts && rm -rf charts/**/tmpcharts'
+}
+
+main() {
+    trap cleanup EXIT
+    cleanup
+    docker run --rm -it -v $(pwd):/repo --workdir /repo quay.io/helmpack/chart-testing ct lint --config ct.yaml "$@"
+}
+
+main "$@"
